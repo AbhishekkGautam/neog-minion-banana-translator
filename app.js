deda89ae8d0f32e8b4d9588bd7628d2b8dd40eae
AbhishekkGautam <abhishekgautm243@gmail.com>
2021-09-13 21:53:53 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,27 +1,27 @@
-const serverURL = "https://api.funtranslations.com/translate/minion.json"
+const serverURL = "https://api.funtranslations.com/translate/minion.json";
 
 var inputTextArea = document.querySelector("#text-input");
 var btnTranslate = document.querySelector("#btn-translate");
 var outputDiv = document.querySelector("#output-div");
 
 function urlWithText(text) {
-	return `${serverURL}?text=${text}`
+  return `${serverURL}?text=${text}`;
 }
 
 function errorHandler(error) {
-	console.log("Error Occured", error)
+  outputDiv.innerHTML = `<p style="color: red;">Some error occured: ${error.message}</p>
+  `;
 }
 
 function btnTranslateHandler() {
-	let inputText = inputTextArea.value;
-	fetch(urlWithText(inputText))
-		.then(response => response.json())
-		.then(json => {
-			let translatedText = json.contents.translated;
-			outputDiv.innerText = translatedText;
-		})
-		.catch(errorHandler)
+  let inputText = inputTextArea.value;
+  fetch(urlWithText(inputText))
+    .then((response) => response.json())
+    .then((json) => {
+      let translatedText = json.contents.translated;
+      outputDiv.innerText = translatedText;
+    })
+    .catch(errorHandler);
 }
 
 btnTranslate.addEventListener("click", btnTranslateHandler);
-
